2780dc58be20b800dbe6aeb4d6a246aa1e8b0e39
1Mao-H14 <139054980+1Mao-H14@users.noreply.github.com>
2023-12-13 21:26:44 +0100

    Update shell _ project _autre

--- shell.h
+++ shell.h
@@ -112,7 +112,7 @@ typedef struct builtin
 
 
 /* hsh.c */
-int hsh(info_t *, char **);
+int shell_fun(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
